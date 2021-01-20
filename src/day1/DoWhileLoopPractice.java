a96734f4f95f6d31e802331cc9957fee0d4b2430
senpolt <senpolt@gmail.com>
2021-01-20 01:15:46 -0600

    DoWhileLoopPractice

---
+++ src/day1/DoWhileLoopPractice.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class DoWhileLoopPractice {
+    public static void main(String[] args) {
+        int x =0;
+        do {
+            System.out.println(x);
+            x++;
+
+        }while (x<9);
+    }
+}
