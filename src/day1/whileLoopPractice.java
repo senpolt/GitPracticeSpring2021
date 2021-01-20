ad55eafce71483b6cfc0aced708f3dc87f25ad0f
senpolt <senpolt@gmail.com>
2021-01-20 01:12:54 -0600

    whileLoopPractice

---
+++ src/day1/whileLoopPractice.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class whileLoopPractice {
+    public static void main(String[] args) {
+        int x=0;
+        while (x<5){
+            System.out.println(x);
+            x++;
+        }
+    }
+}
